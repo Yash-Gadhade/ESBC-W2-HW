98cf4c557a527b5d6ac2e246a157d9d66b721393
Sidhant Nagpal <sidnagpal2000@gmail.com>
2022-11-15 21:34:39 +0530

    updated Delegate.ts

--- scripts/Delegate.ts
+++ scripts/Delegate.ts
@@ -9,11 +9,16 @@ dotenv.config()
 const showPrompt = prompt();
 
 async function main() {
-    const accounts: SignerWithAddress[] = await ethers.getSigners();
-    const signer = accounts[0];
+    const provider = ethers.getDefaultProvider("goerli", {
+        infura: process.env.INFURA_API_KEY, 
+        alchemy: process.env.ALCHEMY_API_KEY, 
+        etherscan: process.env.ETHERSCAN_API_KEY
+    });
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+    const signer = wallet.connect(provider);
     const balanceBN = await signer.getBalance();
-    console.log(`Connected to account of address ${signer.address}
-    \n This account has a balance of ${balanceBN.toString()} Wei`);
+    console.log(`Connected to account of address ${signer.address} 
+    \n This account has a balance of${balanceBN.toString()} Wei`);
 
     const contractAddress = showPrompt("Please enter the contract address: ");
     let ballotContract: Ballot;
@@ -24,7 +29,8 @@ async function main() {
     const tx = await ballotContract.delegate(targetAccount);
     console.log("Waiting for transaction confirmation...");
     const receipt = await tx.wait();
-    console.log(`TRANSACTION SUCCESSFULL!\n ${receipt}`);
+    console.log("TRANSACTION SUCCESSFULL!");
+    console.log(receipt);
 }
 
 main().catch((error) => {
